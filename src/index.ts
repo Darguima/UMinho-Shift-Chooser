945acb480d760d2c6bf9d8b53f494939e26e9ced
Edgar Araújo <edgararaj@gmail.com>
2022-09-16 04:07:00 +0100

    Individual cells are now time aware

--- src/index.ts
+++ src/index.ts
@@ -15,52 +15,87 @@ const createParagraph = (text: string, parent: HTMLElement) => {
   return paragraph
 }
 
+const incrementTime = (time: Time, increment: Time) => {
+  const minute = increment.hour * 60 + increment.minute
+  time.hour += Math.floor((time.minute + minute) / 60)
+  time.minute = (time.minute + minute) % 60
+}
+
+const getElementByXpath = (path: string) => {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+const zeroPad = (num: number, places: number) => String(num).padStart(places, '0')
+
 const main = () => {
-  const extractedClassesContainer = Array.from(document.querySelectorAll<HTMLDivElement>(".rsApt"))
+  const tableHeight = document.querySelector<HTMLDivElement>(".rsContentScrollArea")?.offsetHeight
 
-  const classes: Array<Class> = extractedClassesContainer.map((classContainer) => {
-    let classInfo = Array.from(classContainer.querySelectorAll<HTMLDivElement>(".rsAptContent"))[0]
+  const cellHeight = document.querySelector<HTMLTableRowElement>(".rsContentTable tr")?.offsetHeight
+  if (!cellHeight) return;
 
-    const [subject, location, shift] = classInfo.innerText.split("\n")
-    
-    const _class = document.createElement("div");
+  let classes = Array<Class>()
 
-    classContainer.style.overflow = "hidden"
-    classContainer.style.border = "none"
-    classContainer.style.borderRight = "1px solid #444"
+  // get first hour row
+  const firstHour = getElementByXpath("/html/body/form/div[12]/div/div[4]/div[1]/div[3]/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div[3]/div[4]/div[2]/div/div[2]/table/tbody/tr[2]/td[1]/div/div/table/tbody/tr[1]/th/div")?.textContent?.split(":")
+  if (!firstHour) return;
+  let startTime: Time = {hour: +firstHour[0], minute: +firstHour[1]}
 
-    _class.style.height = "100%"
-    _class.style.width = "100%"
+  document.querySelectorAll<HTMLTableRowElement>(".rsContentTable tr").forEach((row) => {
+    row.querySelectorAll<HTMLDivElement>(".rsApt").forEach((classContainer) => {
+      let classInfo = Array.from(classContainer.querySelectorAll<HTMLDivElement>(".rsAptContent"))[0]
 
-    _class.style.display = "flex"
-    _class.style.flexDirection = "column"
-    _class.style.alignItems = "center"
-    _class.style.justifyContent = "center"
+      const [subject, location, shift] = classInfo.innerText.split("\n")
 
-    const subjectParagraph = createParagraph(subject, _class);
-    const locationParagraph = createParagraph(location, _class);
-    const shiftParagraph = createParagraph(shift, _class);
+      const _class = document.createElement("div");
 
-    classContainer.replaceChildren(_class)
+      classContainer.style.overflow = "hidden"
+      classContainer.style.border = "none"
+      classContainer.style.borderRight = "1px solid #444"
 
-    return {
-      subject,
-      subjectParagraph,
+      _class.style.height = "100%"
+      _class.style.width = "100%"
 
-      location,
-      locationParagraph,
+      _class.style.display = "flex"
+      _class.style.flexDirection = "column"
+      _class.style.alignItems = "center"
+      _class.style.justifyContent = "center"
 
-      shift,
-      shiftParagraph,
+      const durationInMinutes = ((classContainer.offsetHeight + 4) / cellHeight) * 30
+      const duration = {hour: 0, minute: 0}
+      incrementTime(duration, {hour: 0, minute: durationInMinutes})
 
-      shiftType: shift.replace(/[^a-z]/gi, ''),
-      shiftNumber: Number(shift.replace(/[^1-9]/gi, '') || "1"),
+      const endTime = {hour: startTime.hour, minute: startTime.minute}
+      incrementTime(endTime, duration)
 
-      domElement: _class,
-      parentElement: classContainer,
+      const subjectParagraph = createParagraph(subject, _class);
+      const locationParagraph = createParagraph(location, _class);
+      const shiftParagraph = createParagraph(shift, _class);
+      const timeParagraph = createParagraph(`${startTime.hour}:${zeroPad(startTime.minute, 2)} - ${endTime.hour}:${zeroPad(endTime.minute, 2)}`, _class);
 
-      status: "normal"
-    }
+      classContainer.replaceChildren(_class);
+
+      classes.push({
+        subject,
+        subjectParagraph,
+
+        location,
+        locationParagraph,
+
+        shift,
+        shiftParagraph,
+
+        shiftType: shift.replace(/[^a-z]/gi, ''),
+        shiftNumber: Number(shift.replace(/[^1-9]/gi, '') || "1"),
+
+        domElement: _class,
+        parentElement: classContainer,
+
+        status: "normal",
+        startTime,
+        duration
+      })
+    })
+    incrementTime(startTime, {hour: 0, minute: 30})
   })
 
   console.log(classes)
