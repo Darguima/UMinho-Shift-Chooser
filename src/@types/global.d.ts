4464d11e4402babe12d7b4e5276a467e4919ed32
Darguima <dsgdevbraga@gmail.com>
2022-09-17 16:47:56 +0100

    Code refactoring

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -1,29 +1,31 @@
-export {};
+export {}
 
 declare global {
-	interface Time {
-		hour: number;
-		minute: number;
-	}
+  interface Time {
+    hour: number
+    minute: number
+  }
 
-	interface Class {
-		subject: string,
-		subjectParagraph: HTMLParagraphElement,
-	
-		location: string,
-		locationParagraph: HTMLParagraphElement,
-	
-		shift: string,
-		shiftParagraph: HTMLParagraphElement,
-	
-		shiftType: string,
-		shiftNumber: number,
-	
-		domElement: HTMLDivElement,
-		parentElement: HTMLDivElement,
+  interface Class {
+    subject: string
+    subjectParagraph: HTMLParagraphElement
 
-		status: "normal" | "selected" | "hidden",
-		startTime: Time,
-		duration: Time
-	}
+    location: string
+    locationParagraph: HTMLParagraphElement
+
+    shift: string
+    shiftParagraph: HTMLParagraphElement
+
+    startTime: Time
+    endTime: Time
+    timeParagraph: HTMLParagraphElement
+
+    shiftType: string
+    shiftNumber: number
+
+    domElement: HTMLDivElement
+    parentElement: HTMLDivElement
+
+    status: 'normal' | 'selected' | 'hidden'
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,154 +1,162 @@
 const COLORS = {
-  sameSubject: "#00000044",
-  hoverShift: "#ff0000aa",
-  selectedShift: "#808000ff",
-  coloredArray: ['#3cb44b', '#ffe119', '#4363d8', '#f58231', '#f032e6', '#008080', '#9a6324', '#aaffc3',  '#000075', '#bcf60c'] 
+  sameSubject: '#00000044',
+  hoverShift: '#ff0000aa',
+  selectedShift: '#808000ff',
+  coloredArray: ['#3cb44b', '#ffe119', '#4363d8', '#f58231', '#f032e6', '#008080', '#9a6324', '#aaffc3', '#000075', '#bcf60c']
 }
 
-const createParagraph = (text: string, parent: HTMLElement) => {
-  const paragraph = document.createElement("p");
+const createParagraph = (text: string, parent: HTMLElement): HTMLParagraphElement => {
+  const paragraph = document.createElement('p')
 
-  paragraph.style.textAlign = "center"
+  paragraph.style.textAlign = 'center'
   paragraph.innerText = text
   parent.appendChild(paragraph)
 
   return paragraph
 }
 
-const incrementTime = (time: Time, increment: Time) => {
-  const minute = increment.hour * 60 + increment.minute
-  time.hour += Math.floor((time.minute + minute) / 60)
-  time.minute = (time.minute + minute) % 60
+const incrementTime = (initialTime: Time, elapsedTimeInMinutes: number): Time => {
+  // const finalTimeInMinutes = (initial Time In Minutes) + elapsedTimeInMinutes
+  const finalTimeInMinutes = (initialTime.hour * 60 + initialTime.minute) + elapsedTimeInMinutes
+
+  return {
+    hour: Math.floor(finalTimeInMinutes / 60),
+    minute: finalTimeInMinutes % 60
+  }
 }
 
-const getElementByXpath = (path: string) => {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-}
+const zeroPad = (num: number, places: number): string => String(num).padStart(places, '0')
 
-const zeroPad = (num: number, places: number) => String(num).padStart(places, '0')
+const main = (): void => {
+  const cellHeight = document.querySelector<HTMLTableRowElement>('.rsContentTable tr')?.offsetHeight
 
-const main = () => {
-  const tableHeight = document.querySelector<HTMLDivElement>(".rsContentScrollArea")?.offsetHeight
+  if (cellHeight === undefined) {
+    // alert('UMinho Shift Chooser - Error getting the Cell Height. Extension disabled!')
+    // This alert can't be shown because before choose the course is no cells to select
+    return
+  }
 
-  const cellHeight = document.querySelector<HTMLTableRowElement>(".rsContentTable tr")?.offsetHeight
-  if (!cellHeight) return;
+  const firstHourOnSchedule = document.querySelector<HTMLDivElement>('.rsVerticalHeaderTable tbody tr th div')?.innerText.split(':')
 
-  let classes = Array<Class>()
+  if (firstHourOnSchedule === undefined) {
+    alert('UMinho Shift Chooser - Error getting the first hour on schedule. Extension disabled!')
+    return
+  }
 
-  // get first hour row
-  const firstHour = getElementByXpath("/html/body/form/div[12]/div/div[4]/div[1]/div[3]/div[1]/div[3]/div/div[2]/div/div/div[1]/div/div/div/div[3]/div[4]/div[2]/div/div[2]/table/tbody/tr[2]/td[1]/div/div/table/tbody/tr[1]/th/div")?.textContent?.split(":")
-  if (!firstHour) return;
-  let startTime: Time = {hour: +firstHour[0], minute: +firstHour[1]}
+  const firstTime: Time = {
+    hour: +firstHourOnSchedule[0],
+    minute: +firstHourOnSchedule[1]
+  }
 
-  document.querySelectorAll<HTMLTableRowElement>(".rsContentTable tr").forEach((row) => {
-    row.querySelectorAll<HTMLDivElement>(".rsApt").forEach((classContainer) => {
-      let classInfo = Array.from(classContainer.querySelectorAll<HTMLDivElement>(".rsAptContent"))[0]
+  const extractedRows = Array.from(document.querySelectorAll<HTMLTableRowElement>('.rsContentTable tr'))
 
-      const [subject, location, shift] = classInfo.innerText.split("\n")
+  const classes: Class[] = extractedRows
+    .map((row, index) => {
+      // elapsed time in minutes = index * 30 -> each row is a 30 min block
+      const startTime = incrementTime(firstTime, index * 30)
 
-      const _class = document.createElement("div");
+      const extractedClasses: HTMLDivElement[] = Array.from(row.querySelectorAll<HTMLDivElement>('.rsApt'))
 
-      classContainer.style.overflow = "hidden"
-      classContainer.style.border = "none"
-      classContainer.style.borderRight = "1px solid #444"
+      return extractedClasses.map(classContainer => {
+        const classInfo = Array.from(classContainer.querySelectorAll<HTMLDivElement>('.rsAptContent'))[0]
 
-      _class.style.height = "100%"
-      _class.style.width = "100%"
+        const [subject, location, shift] = classInfo.innerText.split('\n')
 
-      _class.style.display = "flex"
-      _class.style.flexDirection = "column"
-      _class.style.alignItems = "center"
-      _class.style.justifyContent = "center"
+        const _class = document.createElement('div')
 
-      const durationInMinutes = ((classContainer.offsetHeight + 4) / cellHeight) * 30
-      const duration = {hour: 0, minute: 0}
-      incrementTime(duration, {hour: 0, minute: durationInMinutes})
+        classContainer.style.overflow = 'hidden'
+        classContainer.style.border = 'none'
+        classContainer.style.borderRight = '1px solid #444'
 
-      const endTime = {hour: startTime.hour, minute: startTime.minute}
-      incrementTime(endTime, duration)
+        _class.style.height = '100%'
+        _class.style.width = '100%'
 
-      const subjectParagraph = createParagraph(subject, _class);
-      const locationParagraph = createParagraph(location, _class);
-      const shiftParagraph = createParagraph(shift, _class);
-      const timeParagraph = createParagraph(`${startTime.hour}:${zeroPad(startTime.minute, 2)} - ${endTime.hour}:${zeroPad(endTime.minute, 2)}`, _class);
+        _class.style.display = 'flex'
+        _class.style.flexDirection = 'column'
+        _class.style.alignItems = 'center'
+        _class.style.justifyContent = 'center'
 
-      classContainer.replaceChildren(_class);
+        const durationInMinutes = ((classContainer.offsetHeight + 4) / cellHeight) * 30
 
-      classes.push({
-        subject,
-        subjectParagraph,
+        const endTime = incrementTime(startTime, durationInMinutes)
 
-        location,
-        locationParagraph,
+        const subjectParagraph = createParagraph(subject, _class)
+        const locationParagraph = createParagraph(location, _class)
+        const shiftParagraph = createParagraph(shift, _class)
+        const timeParagraph = createParagraph(`${zeroPad(startTime.hour, 2)}:${zeroPad(startTime.minute, 2)} - ${zeroPad(endTime.hour, 2)}:${zeroPad(endTime.minute, 2)}`, _class)
 
-        shift,
-        shiftParagraph,
+        classContainer.replaceChildren(_class)
 
-        shiftType: shift.replace(/[^a-z]/gi, ''),
-        shiftNumber: Number(shift.replace(/[^1-9]/gi, '') || "1"),
+        return {
+          subject,
+          subjectParagraph,
 
-        domElement: _class,
-        parentElement: classContainer,
+          location,
+          locationParagraph,
 
-        status: "normal",
-        startTime,
-        duration
+          shift,
+          shiftParagraph,
+
+          startTime,
+          endTime,
+          timeParagraph,
+
+          shiftType: shift.replace(/[^a-z]/gi, ''),
+          shiftNumber: Number(shift.replace(/^\D+/g, '') || '1'),
+
+          domElement: _class,
+          parentElement: classContainer,
+
+          status: 'normal' as const
+        }
       })
     })
-    incrementTime(startTime, {hour: 0, minute: 30})
-  })
+    .flat(1)
 
   console.log(classes)
 
   classes.forEach(selectedClass => {
-    selectedClass.domElement.addEventListener("mouseenter", () => {
-      
+    selectedClass.domElement.addEventListener('mouseenter', () => {
       classes.forEach(_class => {
-        if (_class.subject == selectedClass.subject) {
-
-            if (_class.shiftType == selectedClass.shiftType) {
-
-              if (_class.shiftNumber == selectedClass.shiftNumber) {
-                _class.domElement.style.backgroundColor = COLORS.hoverShift
-              } else {
-                _class.domElement.style.backgroundColor = COLORS.coloredArray[_class.shiftNumber - 1]
-              }
-
-            } else if (selectedClass.status === "normal" && _class.status === "normal") {
-              _class.domElement.style.backgroundColor = COLORS.sameSubject
+        if (_class.subject === selectedClass.subject) {
+          if (_class.shiftType === selectedClass.shiftType) {
+            if (_class.shiftNumber === selectedClass.shiftNumber) {
+              _class.domElement.style.backgroundColor = COLORS.hoverShift
+            } else {
+              _class.domElement.style.backgroundColor = COLORS.coloredArray[_class.shiftNumber - 1]
             }
+          } else if (selectedClass.status === 'normal' && _class.status === 'normal') {
+            _class.domElement.style.backgroundColor = COLORS.sameSubject
+          }
         }
       })
     })
 
-    selectedClass.domElement.addEventListener("mouseleave", () => {
+    selectedClass.domElement.addEventListener('mouseleave', () => {
       classes.forEach((_class) => {
-        _class.domElement.style.backgroundColor = _class.status !== "selected" ? "" : COLORS.selectedShift
+        _class.domElement.style.backgroundColor = _class.status !== 'selected' ? '' : COLORS.selectedShift
       })
     })
   })
 
-
   classes.forEach(selectedClass => {
-    selectedClass.domElement.addEventListener("mouseup", () => {
-      
+    selectedClass.domElement.addEventListener('mouseup', () => {
       classes.forEach(_class => {
-        if (_class.subject == selectedClass.subject) {
-          if (_class.shiftType == selectedClass.shiftType) {
-            if (_class.shiftNumber == selectedClass.shiftNumber) {
+        if (_class.subject === selectedClass.subject) {
+          if (_class.shiftType === selectedClass.shiftType) {
+            if (_class.shiftNumber === selectedClass.shiftNumber) {
               _class.domElement.style.backgroundColor = COLORS.selectedShift
-              _class.parentElement.style.opacity = "1"
-  
-              _class.status = "selected"
+              _class.parentElement.style.opacity = '1'
+
+              _class.status = 'selected'
             } else {
-              _class.parentElement.style.opacity = "0.4"
-  
-              _class.status = "hidden"
+              _class.parentElement.style.opacity = '0.4'
+
+              _class.status = 'hidden'
             }
           } else {
-            _class.domElement.style.backgroundColor = ""
+            _class.domElement.style.backgroundColor = ''
           }
-          
         }
       })
     })
